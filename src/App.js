250632bc1bd14f246f39ed44c892883d93e3ba79
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-09-19 18:48:10 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-
-import './App.css';
-import Counter from './components/Counter';
-import CounterBasedOnPreviousState from './components/CounterBasedOnPreviousState';
-import HookCounterThree from './components/hookcounterthree';
-import UseStateWithArrays from './components/useStateWithArrays';
-import UseEffect from './components/UseEffect';
-import Mouse from './components/Mouse';
-import UseEffectCleaUp from './components/UseEffectCleaUp';
-import IntervalCounter from './components/IntervalCounter';
+import "./App.css";
+import Counter from "./components/Counter";
+import CounterBasedOnPreviousState from "./components/CounterBasedOnPreviousState";
+import HookCounterThree from "./components/hookcounterthree";
+import UseStateWithArrays from "./components/useStateWithArrays";
+import UseEffectCleaUp from "./components/UseEffectCleaUp";
+import UseEffect from "./components/UseEffect";
+import Mouse from "./components/Mouse";
+import IntervalCounter from "./components/IntervalCounter";
+import FetchData from "./components/FetchData";
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <CounterBasedOnPreviousState/>
-      <HookCounterThree/>
-      <UseStateWithArrays/>
-      <UseEffect/>
+      <Counter />
+      <CounterBasedOnPreviousState />
+      <HookCounterThree />
+      <UseStateWithArrays />
+      <UseEffect />
       {/* <Mouse/> */}
-      <UseEffectCleaUp/>
-    <IntervalCounter/>
+      <UseEffectCleaUp />
+      <IntervalCounter />
+      <FetchData />
     </div>
   );
 }
