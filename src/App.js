57552901d367d239b87f3a850c34ae4e1c585206
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-09-19 20:11:03 -0600

    added fetch data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
 import Counter from "./components/Counter";
 import CounterBasedOnPreviousState from "./components/CounterBasedOnPreviousState";
 import HookCounterThree from "./components/hookcounterthree";
+import IntervalCounter from "./components/IntervalCounter";
 import UseStateWithArrays from "./components/useStateWithArrays";
 import UseEffectCleaUp from "./components/UseEffectCleaUp";
 import UseEffect from "./components/UseEffect";
+import UseReducer from "./components/UseReducer";
 import Mouse from "./components/Mouse";
-import IntervalCounter from "./components/IntervalCounter";
 import FetchData from "./components/FetchData";
+
+import "./App.css";
+import UseReducer2 from "./useReducer/UseReducer";
+import DataFetching from "./useReducer/DataFetching";
 
 function App() {
   return (
@@ -21,6 +25,9 @@ function App() {
       <UseEffectCleaUp />
       <IntervalCounter />
       <FetchData />
+      <UseReducer/>
+      <UseReducer2/>
+      <DataFetching/>
     </div>
   );
 }
